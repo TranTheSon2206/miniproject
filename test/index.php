5d1c8467212f12aab348e71d754231d4c0f45c52
TranTheSon2206 <109707502+TranTheSon2206@users.noreply.github.com>
2023-06-14 09:35:06 +0700

    Add files via upload

---
+++ test/index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+  <title>Mini Project</title>
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a type="button" class="btn btn-primary nav-link active" href="create.php">Create a new</a>
+          </li>
+        </ul>
+   
+    </div>
+  </nav>
+  <div class="container my-4">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>NAME</th>
+          <th>EMAIL</th>
+          <th>PHONE</th>
+          <th>JOINING DATE</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        include "config.php";
+        $sql = "select * from employee";
+        $result = $conn->query($sql);
+        if (!$result) {
+          die("Invalid query!");
+        }
+        while ($row = $result->fetch_assoc()) {
+          echo "
+      <tr>
+        <th>$row[id]</th>
+        <td>$row[name]</td>
+        <td>$row[email]</td>
+        <td>$row[phone]</td>
+        <td>$row[join_date]</td>
+      </tr>
+      ";
+        }
+        ?>
+      </tbody>
+    </table>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
